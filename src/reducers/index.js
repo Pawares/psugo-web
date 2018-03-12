648e890bee53d84aca11795d8f9fd5b52e85209a
iPoon <pawares.r@gmail.com>
2018-03-13 00:40:21 +0700

    Redux Form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
 import MissionsReducer from './reducer_missions'
 
 const rootReducer = combineReducers({
-    missions: MissionsReducer
+    missions: MissionsReducer,
+    form: formReducer
 })
 
 export default rootReducer
