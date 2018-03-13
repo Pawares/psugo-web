8f9443a637958fe74e00362f9d67e7ac70cf5f15
iPoon <pawares.r@gmail.com>
2018-03-13 22:19:43 +0700

    Fix reducers bugs

--- src/reducers/reducer_missions.js
+++ src/reducers/reducer_missions.js
@@ -5,8 +5,9 @@ export default function(state = {}, action) {
     switch (action.type) {
         case FETCH_MISSIONS: return _.mapKeys(action.payload, 'id')
         case CREATE_MISSION: return state
-        case FETCH_MISSION: return _.mapKeys(action.payload, 'id')
-        case DELETE_MISSION: return _.omit(state, action.payload)
+        case FETCH_MISSION: return { ...state, [action.payload.id] : _.mapKeys(action.payload, 'id')}
+        // case FETCH_MISSION: return { ...state, [action.payload.id] : _.mapKeys(action.payload, 'id')}
+        case DELETE_MISSION: return _.omit(state, [action.payload])
         default: return state
     }
 }
